14b698f5335567349e1d978e0831410e340c95a0
Bronson Manley <brmanley@outlook.com>
2026-04-28 12:27:12 -0400

    add color + transform support (rect/circle RGB + rotate/translate)

--- examples/test.mc
+++ examples/test.mc
@@ -1,6 +1,11 @@
 (begin
   (list
-    (list "circle"
-      (add 200 (mul 100 (sin time)))
-      200
-      30)))
+    (list "rect" 50 50 100 100 255 0 0)
+    (list "circle" 200 200 40 0 0 255)
+    (list "translate"
+      (mul 100 (sin time))
+      0
+      (list "circle" 400 300 30 255 255 0))
+    (list "rotate"
+      time
+      (list "rect" 350 250 80 80 0 255 0))))
--- renderer.js
+++ renderer.js
@@ -8,15 +8,44 @@ export function render(commands) {
     const [type, ...args] = cmd;
 
     if (type === "rect") {
-      const [x, y, w, h] = args;
+      const [x, y, w, h, r=0, g=0, b=0] = args;
+
+      ctx.fillStyle = `rgb(${r},${g},${b})`;
       ctx.fillRect(x, y, w, h);
     }
 
     if (type === "circle") {
-      const [x, y, r] = args;
+      const [x, y, radius, r=0, g=0, b=0] = args;
+
+      ctx.fillStyle = `rgb(${r},${g},${b})`;
+
       ctx.beginPath();
-      ctx.arc(x, y, r, 0, Math.PI * 2);
+      ctx.arc(x, y, radius, 0, Math.PI * 2);
       ctx.fill();
+    }
+
+    if (type === "rotate") {
+      const [angle, inner] = args;
+
+      ctx.save();
+      ctx.translate(400, 300);
+      ctx.rotate(angle);
+      ctx.translate(-400, -300);
+
+      render([inner]);
+
+      ctx.restore();
+    }
+
+    if (type === "translate") {
+      const [tx, ty, inner] = args;
+
+      ctx.save();
+      ctx.translate(tx, ty);
+
+      render([inner]);
+
+      ctx.restore();
     }
   }
 }
